e294499d3c54c740c67666276798fe0dafa37fdf
ogeokoli <88599087+ogeokoli@users.noreply.github.com>
2022-05-28 22:43:52 +0100

    Add files via upload

---
+++ CLASS AND OBJECT.py
@@ -0,0 +1,28 @@
+class Student:
+    person = 'student'
+
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+        # pass
+Bob = Student("Bob", int(26), ["FE","BE"],float(20.90))
+
+Bob.name = "Peter"
+Bob.age = int(34)
+Bob.tracks = ["FE", "BE", "GE"]
+
+print('Student details:')
+print('The student\'s name is ', Bob.name)
+print('age: ', Bob.age)
+print('tracks: ', Bob.tracks)
+print('score: ', Bob.score)
+
+
+
+# Expected methods
+# Bob.change_name("Peter")
+# Bob.change_age(34)
+# Bob.add_track("UI/UX")
+# Bob.get_score()
